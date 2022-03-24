111d586cfadf08a054072b3ae765a5d57ab4e210
goncalveshigino <goncalveshigino20@gmail.com>
2022-03-24 06:49:38 +0100

    chore: add ejs

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs');
+
 app.get('/', ( req, res) => res.send('Ola mundo!'));
+
+app.get('/produtos', ( req, res) => {
+    res.render('produtos', {title: 'Lista de Produtos'})
+});
 
 app.listen(port, () => console.log(`Aplicacao rodando na porta ${port}.`))
